50b2b988541cc6709f573c79abff07d33bb70f8f
ChaGPT-33 <clarie3chagpt@gmail.com>
2025-03-19 15:29:34 +0800

    Add files via upload
    change pyspark to pandas, easier execution

--- src/DMOP.py
+++ src/DMOP.py
@@ -1,103 +1,87 @@
 #%%
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum, regexp_replace
 import pandas as pd
 import os
 from dotenv import load_dotenv
 from utils import MySQLUtils
 import numpy as np
-from multiprocessing import Process, current_process, Queue
+from multiprocessing import Pool, freeze_support
+#%% using pandas to process the data 
+def load_file(file_path):
+    # Read DMOP Excel files using pandas
+    dmop_cols = ["CompanyCode", "DocumentNo", "SAPInoviceNo", "SAPBillingDate", "SAPPositingDate", 
+        "SAPTaxCode", "SAPOrderNo", "SAPDispatchNoteNo", "ProductLine", "SAPStorageLocation", "SAPRoute", 
+        "Plant", "SAPCustomerTaxClassification", "SAPMaterialTaxClassification", "SAPIncoterm", "ShipToName", 
+        "ShipToCountry", "CountryKey", "SoldToName", "CurrencyCode", "UM_ST", "UM_EURO", "UM_WAE", 
+        "UM_TAX", "UM_TAX_WAE", "SAPTaxDepartCountry", "CostBaseSGD","GST_VAT_SGD"]
 
-# Connect to database
-load_dotenv()
-mysql_conn = MySQLUtils(
-    driver = os.getenv("DRIVER_NAME"),
-    db_user = os.getenv("DB_USERNAME"),
-    db_password = os.getenv("DB_PASSWORD"),
-    host = os.getenv("HOST"),
-    port = os.getenv("PORT"),
-    given_db = os.getenv("DATABASE")
-)
-mysql_conn.connect_to_mysql()
+    dmop_pd = pd.read_excel(file_path, usecols='B,E:F,H:K,M,Q:W,Y:AA,AD,AF:AK,AN:AP')
+    dmop_pd = dmop_pd.set_axis(dmop_cols, axis=1)
 
-#%% using pyspark to process the data 
-# Initialize Spark session with
-spark = SparkSession.builder \
-    .appName("MySQL Data Insertion") \
-    .master("local[*]") \
-    .getOrCreate()
+    # step 1: aggregate the values
+    dmop_agg = dmop_pd.groupby("DocumentNo", as_index=False).agg({
+            "UM_EURO": "sum",
+            "UM_WAE": "sum",
+            "UM_TAX": "sum",
+            "UM_TAX_WAE": "sum"
+        }).rename(columns={
+            "UM_EURO": "UM_EURO",
+                "UM_WAE": "UM_WAE",
+                "UM_TAX": "UM_TAX",
+                "UM_TAX_WAE": "UM_TAX_WAE"
+            })
 
-# Read DMOP Excel files using pandas
-dmop_cols = ["CompanyCode", "DocumentNo", "SAPInoviceNo", "SAPBillingDate", "SAPPositingDate", 
-    "SAPTaxCode", "SAPOrderNo", "SAPDispatchNoteNo", "ProductLine", "SAPStorageLocation", "SAPRoute", 
-    "Plant", "SAPCustomerTaxClassification", "SAPMaterialTaxClassification", "SAPIncoterm", "ShipToName", 
-    "ShipToCountry", "CountryKey", "SoldToName", "CurrencyCode", "UM_ST", "UM_EURO", "UM_WAE", 
-    "UM_TAX", "UM_TAX_WAE", "SAPTaxDepartCountry", "CostBaseSGD","GST_VAT_SGD"]
+    # step 2: join back rest columns to the aggregated data frame to ensure DocumentNo is unique
+    dmop_drop = dmop_pd.drop(columns=["UM_EURO", "UM_WAE", "UM_TAX", "UM_TAX_WAE"])
+    dmop_unique = pd.merge(dmop_agg, dmop_drop, on="DocumentNo", how="left").drop_duplicates(subset=['DocumentNo'])
 
-dmop_pd = pd.read_excel("./asset/DMOP_IFAP_DEC24.xlsx",
-                        usecols='B,E:F,H:K,M,Q:W,Y:AA,AD,AF:AK,AN:AP')
-dmop_pd = dmop_pd.set_axis(dmop_cols, axis=1)
+    # step 3: Replace NaN and clean text columns
+    dmop_unique = dmop_unique.replace(['nan', np.nan], "null")
+    dmop_unique["SoldToName"] = dmop_unique["SoldToName"].str.replace("'", "-")
+    dmop_unique["ShipToName"] = dmop_unique["ShipToName"].str.replace("'", "-")
 
-# Convert pandas DataFrames to Spark DataFrames
-dmop_spark = spark.createDataFrame(dmop_pd)
-
-# step 1: aggregate the values
-dmop_spark_agg = dmop_spark.groupBy("DocumentNo").agg(
-    sum("UM_EURO").alias("UM_EURO"), 
-    sum("UM_WAE").alias('UM_WAE'), 
-    sum("UM_TAX").alias("UM_TAX"), 
-    sum("UM_TAX_WAE").alias("UM_TAX_WAE")
-)
-
-# step 2: join back rest columns to the aggregated data frame to ensure DocumentNo is unique
-dmop_spark_drop = dmop_spark.drop("UM_EURO", "UM_WAE", "UM_TAX", "UM_TAX_WAE", "CountryKey")
-dmop_spark_unique = dmop_spark_agg \
-    .join(dmop_spark_drop, on="DocumentNo", how="left") \
-    .replace(float('nan'), None) \
-    .withColumn("SoldToName", regexp_replace("SoldToName", "'", "-")) \
-    .withColumn("ShipToName", regexp_replace("ShipToName", "'", "-")) \
-
-# step 3: seperate the countryCode table from the main table
-# get columns for countryCode table
-dmop_country_code = dmop_spark.select(col("ShipToCountry").alias('CountryName'), col("CountryKey").alias("CountryKey"))
-# Remove duplicates based on 'ShipToCountry'
-dmop_country_code_no_duplicates = dmop_country_code.dropDuplicates(["CountryName"])
-
-# convert spark df back to pandas df, prepare for data upload
-dmop_spark_unique_df = dmop_spark_unique.toPandas()
-dmop_spark_unique_df = dmop_spark_unique_df.replace(['nan', np.nan], "null")
-dmop_country_code_no_duplicates_df = dmop_country_code_no_duplicates.toPandas()
-spark.stop()
+    # step 4: seperate the countryCode table from the main table
+    # get columns for countryCode table
+    dmop_country_code = dmop_unique[["ShipToCountry", "CountryKey"]].rename(
+        columns={"ShipToCountry": "CountryName", "CountryKey": "CountryKey"}
+    ).drop_duplicates()
+    dmop_unique = dmop_unique.drop(columns=['CountryKey'])
+    return dmop_unique, dmop_country_code
 
 #%% DMOP data upload
-def upload_date(mysql_conn, df, *target_table_info):
-    result = mysql_conn.insert_dataframe_to_sql(df, *target_table_info)
-    return result
+def upload_data(chunk, *target_table_info):
+    mysql_conn = MySQLUtils(
+        driver = "mysql+mysqlconnector",
+        db_user = "", # change to your own 
+        db_password = "", # change to your own
+        host ="", # change to your own
+        port = "3306",
+        given_db ="", # change to your own
+    )
+    mysql_conn.connect_to_mysql()
+    mysql_conn.insert_dataframe_to_sql(chunk, *target_table_info)
 
-def worker_process(q):
-    process_name = current_process().name
-    print(f"{process_name} started")
-
-    country_code_db_info = ('Country', ['CountryName']) #(table_name, primary key list)
-    dmop_main_table_info = ('DMOP', ['DocumentNo'])
-    upload_date(mysql_conn, dmop_country_code_no_duplicates_df, *country_code_db_info)
-    upload_date(mysql_conn, dmop_spark_unique_df, *dmop_main_table_info)
-    q.put(f"{process_name} finished")
+def upload_data_wrapper(args):
+    return upload_data(*args)
 
 def main():
-    q = Queue()
-    processes = []
+    num_processes = 4  # Number of parallel processes
+    country_code_db_info = ('Country', ['CountryName']) #(table_name, primary key list)
+    dmop_main_table_info = ('DMOP', ['DocumentNo'])
 
-    for _ in range(4):  # create 4 processes
-        p = Process(target=worker_process, args=(q,))
-        processes.append(p)
-        p.start()
+    # load folder
+    dmop_folder = './asset/DMOP'
+    for file_name in os.listdir(dmop_folder):
+        file_path = os.path.join(dmop_folder, file_name)
 
-    for p in processes:
-        p.join()
+        dmop_main_table_df, country_code_df = load_file(file_path)
+        upload_data(country_code_df, *country_code_db_info) # no need to use multi-thread as the data size always small for this
 
-    while not q.empty():
-        print(q.get())
+        dmop_chunks = np.array_split(dmop_main_table_df, num_processes)
+        freeze_support()
+        args = [(chunk, *dmop_main_table_info) for chunk in dmop_chunks]
+        with Pool(num_processes) as pool:
+            pool.map(upload_data_wrapper, args)
+
 
 if __name__ == "__main__":
     main()
--- src/tax_report.py
+++ src/tax_report.py
@@ -4,91 +4,86 @@ import os
 from dotenv import load_dotenv
 from utils import MySQLUtils
 import numpy as np
-from multiprocessing import Process, current_process, Queue
-
-# Connect to database
-load_dotenv()
-mysql_conn = MySQLUtils(
-    driver = os.getenv("DRIVER_NAME"),
-    db_user = os.getenv("DB_USERNAME"),
-    db_password = os.getenv("DB_PASSWORD"),
-    host = os.getenv("HOST"),
-    port = os.getenv("PORT"),
-    given_db = os.getenv("DATABASE")
-)
-mysql_conn.connect_to_mysql()
-
+from multiprocessing import Pool, freeze_support
 #%%
-# Read tax reporting excel files 
-tax_report_cols = ['CoCode', 'FIDocNo', 'CustomerNo', 'PostingDate', 'TxRptgDate', 'DocDate', 'DocType', 'InvoiceNo', 
-                   'TaxCode', 'TaxRate', 'GLAccount', 'Currency', 'DocNetValue', 'TaxDocValue', 'ExchRate', 'NetLC', 
-                   'LCTax', 'LocalCurrency', 'CustomerID', 'CustomerName', 'Plant', 'ShipFrom', 'ShipTo', 'CountrySR', 
-                   'ServiceRend', 'DeliveryNote', 'CustomerTaxClassification', 'MaterialTaxClassification']
-text_columns = [
-    "CoCode", "FIDocNo", "CustomerNo", "DocType", "InvoiceNo", "TaxCode", 
-    "GLAccount", "Currency", "LocalCurrency", "Plant", "ShipFrom", "ShipTo", 
-    "CountrySR", "ServiceRend", "DeliveryNote", "CustomerTaxClassification", "MaterialTaxClassification"
-]
-decimal_columns = ["DocNetValue", "TaxDocValue", "NetLC", "LCTax"]
+def load_file(file_path):
+    # Read tax reporting excel files 
+    tax_report_cols = ['CoCode', 'FIDocNo', 'CustomerNo', 'PostingDate', 'TxRptgDate', 'DocDate', 'DocType', 'InvoiceNo', 
+                    'TaxCode', 'TaxRate', 'GLAccount', 'Currency', 'DocNetValue', 'TaxDocValue', 'ExchRate', 'NetLC', 
+                    'LCTax', 'LocalCurrency', 'CustomerID', 'CustomerName', 'Plant', 'ShipFrom', 'ShipTo', 'CountrySR', 
+                    'ServiceRend', 'DeliveryNote', 'CustomerTaxClassification', 'MaterialTaxClassification']
+    text_columns = [
+        "CoCode", "FIDocNo", "CustomerNo", "DocType", "InvoiceNo", "TaxCode", 
+        "GLAccount", "Currency", "LocalCurrency", "Plant", "ShipFrom", "ShipTo", 
+        "CountrySR", "ServiceRend", "DeliveryNote", "CustomerTaxClassification", "MaterialTaxClassification"
+    ]
+    decimal_columns = ["DocNetValue", "TaxDocValue", "NetLC", "LCTax"]
 
+    # load the file under folder
+    tax_report_pd = pd.read_excel(file_path, usecols='A:B,D,F:M,O:AA,AD:AE,AG,AI')
+    tax_report_pd = tax_report_pd.set_axis(tax_report_cols, axis=1)
+    tax_report_pd[text_columns].astype(str)
+    tax_report_pd[decimal_columns].astype(float)
 
-tax_report_pd = pd.read_excel('./asset/IFAP-PIO-DEC24(1-5).xls',
-                              usecols='A:B,D,F:M,O:AA,AD:AE,AG,AI')
-tax_report_pd = tax_report_pd.set_axis(tax_report_cols, axis=1)
-tax_report_pd[text_columns].astype(str)
-tax_report_pd[decimal_columns].astype(float)
+    # data processing
+    # step 1: remove the '/' at currency rate column, remove "'" in customerName column
+    tax_report_pd["TaxRate"] = tax_report_pd["TaxRate"].apply(lambda x: str(x).strip().replace('/', ''))
+    tax_report_pd["ExchRate"] = tax_report_pd["ExchRate"].apply(lambda x: str(x).strip().replace('/', ''))
+    tax_report_pd["CustomerName"] = tax_report_pd["CustomerName"].apply(lambda x: str(x).strip().replace("'", '-'))
+    tax_report_pd["CustomerID"] = tax_report_pd["CustomerID"].apply(lambda x: str(x).strip().replace(".0", ''))
 
-# data processing
-# step 1: remove the '/' at currency rate column, remove "'" in customerName column
-tax_report_pd["TaxRate"] = tax_report_pd["TaxRate"].apply(lambda x: str(x).strip().replace('/', ''))
-tax_report_pd["ExchRate"] = tax_report_pd["ExchRate"].apply(lambda x: str(x).strip().replace('/', ''))
-tax_report_pd["CustomerName"] = tax_report_pd["CustomerName"].apply(lambda x: str(x).strip().replace("'", '-'))
-tax_report_pd["CustomerID"] = tax_report_pd["CustomerID"].apply(lambda x: str(x).strip().replace(".0", ''))
+    # step 2: standardize date format
+    date_columns = ["PostingDate", "TxRptgDate", "DocDate"]
+    for col in date_columns:
+        tax_report_pd[col] = pd.to_datetime(tax_report_pd[col], format='%d.%m.%Y').dt.strftime('%Y-%m-%d')
 
-# step 2: standardize date format
-date_columns = ["PostingDate", "TxRptgDate", "DocDate"]
-for col in date_columns:
-    tax_report_pd[col] = pd.to_datetime(tax_report_pd[col], format='%d.%m.%Y').dt.strftime('%Y-%m-%d')
+    # step 3: replace null value
+    tax_report_pd = tax_report_pd.replace(['nan', np.nan], "null")
 
-# step 3: replace null value
-tax_report_pd = tax_report_pd.replace(['nan', np.nan], "null")
+    # select out customerNo and customerName for 'Customer' table
+    customer_code = tax_report_pd[['CustomerID', 'CustomerName']]
+    customer_code_unique = customer_code.drop_duplicates(subset=['CustomerID'])
 
-# select out customerNo and customerName for 'Customer' table
-customer_code = tax_report_pd[['CustomerID', 'CustomerName']]
-customer_code_unique = customer_code.drop_duplicates(subset=['CustomerID'])
+    # drop the unused cols from main table
+    tax_report_pd = tax_report_pd.drop(columns=["CustomerName"])
 
-# drop the unused cols from main table
-tax_report_pd = tax_report_pd.drop(columns=["CustomerName"])
+    return customer_code_unique, tax_report_pd
 
 #%% Tax Report data upload
-def upload_date(mysql_conn, df, *target_table_info):
-    result = mysql_conn.insert_dataframe_to_sql(df, *target_table_info)
-    return result
+# Connect to database
+def upload_data(chunk, *target_table_info):
+    mysql_conn = MySQLUtils(
+        driver = "mysql+mysqlconnector",
+        db_user = "", # change to your own 
+        db_password = "", # change to your own
+        host ="", # change to your own
+        port = "3306",
+        given_db ="", # change to your own 
+    )
+    mysql_conn.connect_to_mysql()
+    mysql_conn.insert_dataframe_to_sql(chunk, *target_table_info)
 
-def worker_process(q):
-    process_name = current_process().name
-    print(f"{process_name} started")
-
-    customer_code_db_info = ('Customer', ['CustomerID'])
-    tax_report_db_info = ('TaxReporting', ['FIDocNo', 'DocNetValue'])
-    # upload_date(mysql_conn, customer_code_unique, *customer_code_db_info)
-    upload_date(mysql_conn, tax_report_pd, *tax_report_db_info)
-    q.put(f"{process_name} finished")
+def upload_data_wrapper(args):
+    return upload_data(*args)
 
 def main():
-    q = Queue()
-    processes = []
+    num_processes = 4  # Number of parallel processes
+    customer_code_db_info = ('Customer', ['CustomerID'])
+    tax_report_db_info = ('TaxReporting', ['FIDocNo', 'DocNetValue'])
 
-    for _ in range(4):  # create 4 processes
-        p = Process(target=worker_process, args=(q,))
-        processes.append(p)
-        p.start()
+    # load folder
+    tax_report_folder = './asset/TaxReport'
+    for file_name in os.listdir(tax_report_folder):
+        file_path = os.path.join(tax_report_folder, file_name)
 
-    for p in processes:
-        p.join()
+        customer_df, tax_report_df = load_file(file_path)
+        upload_data(customer_df, *customer_code_db_info) # no need to use multi-thread as the data size always small for this
 
-    while not q.empty():
-        print(q.get())
+        tax_report_chunks = np.array_split(tax_report_df, num_processes)
+        freeze_support()
+        args = [(chunk, *tax_report_db_info) for chunk in tax_report_chunks]
+        with Pool(num_processes) as pool:
+            pool.map(upload_data_wrapper, args)
 
 if __name__ == "__main__":
     main()
